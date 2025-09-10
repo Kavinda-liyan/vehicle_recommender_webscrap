36515094644e31ebd4d38d303f2fc5b9fa9523f3
Kavinda <chamarakavinda@hotmail.com>
2025-09-10 21:41:33 +0530

    Enhance product catalog and stock table styling; implement vehicle data scraping

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from bs4 import BeautifulSoup
 with open("index.html", "r", encoding="utf-8") as html_file:
     content = html_file.read()
     soup = BeautifulSoup(content, "lxml")
-    desc_tags = soup.find_all("p")
-    for tag in desc_tags:
-        print(tag.get_text())
-    
+    product_cards = soup.find_all("div", class_="product")
+    for card in product_cards:
+        product_title = card.find("h2", class_="title").text
+        product_description = card.find("p", class_="desc").text
+        product_price = card.find("span", class_="price").text
+
+        print(f"Title:{product_title}")
+        print(f"Description:{product_description}")
+        print(f"Price:{product_price}\n")
